9a389491c2569621f7e64a90dfbbeacc725a4d05
ethiris01 <md.ethiris2904@gmail.com>
2023-09-15 19:24:56 +0530

    fix errors

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -17,8 +17,8 @@ const Landing = () => {
           <p>
             A Job tracking app is a type of software application that helps
             individuals or businesses manage and keep track of jobs. It allows
-            users to monitor the progress of jobs, add new jobs, and even view
-            stats based on their importance.
+            users to monitor the progress of jobs and even view stats based on
+            their importance.
           </p>
           <Link to="/register" className="btn register-link">
             Register
